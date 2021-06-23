6f853b4ca98489be56ffcbc0a8c4fe223ae2d4b5
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-06-23 20:47:45 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,12 +5,6 @@ const { blue } = require('colors'),
 module.exports = (client) => {
     console.log(blue(`- Connexion de ${client.user.tag} réussie.\n- Base de données connectée.`) + "\n==================================================================")
     client.channels.cache.get(botlogs).send("**" + online + " | Je viens tout juste de me connecter !**")
-    
-    client.on("message", (client, message) => {
-        if (message.author.id === '302050872383242240' && message.embeds[0].description.toLowerCase().includes('https://disboard.org')) {
-            return message.channel.send(client.yes + " | Merci à toi pour ton bump !")
-           }
-    })
 
     client.user.setActivity('hémerger d\'un profond sommeil...');
     const activities = [`${client.prefix}help | By Nolhan#2508`, `Version ${client.version} | By Nolhan#2508`];
